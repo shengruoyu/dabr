37402122ddfe4d4946cb9eae8a671eca98cde9a1
david.carrington <david.carrington@760c5f7a-8625-11dd-9205-735403a6da14>
2009-08-11 19:48:18 +0000

    Building up ideas of using external templates and a new front_controller system. Bit fed up of menu_register().

---
+++ dabr_from_scratch/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'theme.php';
+
+function page_home() {
+  $content = '<p>Home</p>';
+  echo theme('page', array(
+    'content' => $content,
+    'title' => 'Home',
+  ));
+}
+
+function front_controller() {
+  $query = (array) explode('/', $_GET['q']);
+  $GLOBALS['page'] = $query[0];
+  $page_function = 'page_'.$GLOBALS['page'];
+  if (!function_exists($page_function)) {
+    $page_function = 'page_home';
+  }
+
+  return call_user_func($page_function, $query);
+}
+
+front_controller();
+
+?>
