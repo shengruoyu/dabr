e63d08a94867b23e8332a159258eef023f330531
david.carrington <david.carrington@760c5f7a-8625-11dd-9205-735403a6da14>
2009-08-11 20:39:00 +0000

    Created two example pages: Twitter trends and OAuth.

--- dabr_from_scratch/templates/mobile/page.tpl.php
+++ dabr_from_scratch/templates/mobile/page.tpl.php
@@ -3,6 +3,10 @@
   <title><?php echo $title; ?></title>
 </head>
 <body>
+<ul>
+  <li><a href="oauth">OAuth test</a></li>
+  <li><a href="trends">Twitter trends API request test</a></li>
+</ul>
 <?php echo $content; ?>
 </body>
 </html>
