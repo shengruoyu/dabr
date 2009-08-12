fe343664368d2bfaf1d8ae20e7466cc36437facd
david.carrington <david.carrington@760c5f7a-8625-11dd-9205-735403a6da14>
2009-08-12 08:18:34 +0000

    Partially commented code

--- branches/davidcarrington/dabr_from_scratch/index.php
+++ branches/davidcarrington/dabr_from_scratch/index.php
@@ -15,15 +15,21 @@ if (!function_exists('page_default')) {
 }
 
 function front_controller() {
+  // Our .htaccess file and ModRewrite send all page requests through here
+  // Find the name of the page to use
   $query = (array) explode('/', $_GET['q']);
-  $GLOBALS['page'] = $query[0];
-  $page_function = 'page_'.$GLOBALS['page'];
+  $page_name = $query[0];
+  
+  // Test if that page exists as a function
+  $page_function = 'page_'.$page_name;
   if (!function_exists($page_function)) {
     $page_function = 'page_default';
   }
 
+  // Call the page function
   $page_args = call_user_func($page_function, $query);
   
+  // If a valid page response was received, render it
   if (is_array($page_args) && array_key_exists('title', $page_args)) {
     $html = theme('page', $page_args);
     ob_start('ob_gzhandler');
@@ -31,6 +37,7 @@ function front_controller() {
     echo $html;
   }
   
+  // That's all folks
   exit();
 }
 
