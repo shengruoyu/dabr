2d43cc37cf44af142de1fc0df4a9b931b5a6f2b1
david.carrington <david.carrington@760c5f7a-8625-11dd-9205-735403a6da14>
2009-08-13 08:52:57 +0000

    More commented code. Removed old theme_*() style code in favour of using more templates.

--- dabr_from_scratch/includes/theme.php
+++ dabr_from_scratch/includes/theme.php
@@ -1,34 +1,40 @@
 <?php
 
 function theme($template, $args) {
+  // Cached list of templates available
   static $templates = array();
   
+  // Hard coded theme for now. This would be in settings
   $current_theme = 'mobile';
   
   $template_directory = 'templates/'.$current_theme;
   if (empty($templates)) {
+    // Populate the $templates cache with a quick directory scan
     $dh  = opendir($template_directory);
     while (false !== ($filename = readdir($dh))) {
+      // Only .tpl.php files are valid templates
       if (substr($filename, -8) == '.tpl.php')
       $templates[] = substr($filename, 0, -8);
     }
   }
+  
+  // Check if the chosen template exists
   if (in_array($template, $templates)) {
+    // Extract function is magic, converts everything in $args into
+    // variables for the template to use.
     extract($args, EXTR_SKIP);
+    
+    // Include the template and return the HTML
+    // No HTML is rendered to the client here because of the ob_get_contents()
     ob_start(); 
     include $template_directory.'/'.$template.'.tpl.php';
-    $output = ob_get_contents();
+    $html = ob_get_contents();
     ob_end_clean();
-    return $output;
+    return $html;
   } else {
-    $function = 'theme_'.$template;
-    $custom_function = $current_theme.'_'.$function;
-    if (!function_exists($custom_function))
-      $function = $custom_function;
-    if (!function_exists($function)) return "<p>Error: theme template <b>$template</b> not found.</p>";
-    return call_user_func_array($function, $args);
+    // Template doesn't exist in our cache
+    trigger_error("<p>Error: template <b>$template</b> not found.</p>");
   }
-  return $output;
 }
 
 ?>
