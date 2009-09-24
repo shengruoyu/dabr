8bbe54705f02b7c3fb4c8d32df9b9bc0cf1650c3
david.carrington <david.carrington@760c5f7a-8625-11dd-9205-735403a6da14>
2009-09-24 21:12:56 +0000

    Added a timer, timeouts, and removed "avatar" text from broken profile pictures.

--- branches/davidcarrington/dabr_from_scratch/templates/mobile/timeline.tpl.php
+++ branches/davidcarrington/dabr_from_scratch/templates/mobile/timeline.tpl.php
@@ -2,7 +2,7 @@
 <tbody>
 <?php foreach ($timeline as $tweet): ?>
 <tr class="<?php echo ($i++ %2 ? 'even' : 'odd'); ?>">
-  <td><img src="<?php echo $tweet->from->profile_image_url ?>" alt="avatar" height="24" width="24" /></td>
+  <td><img src="<?php echo $tweet->from->profile_image_url ?>" alt="" height="24" width="24" /></td>
   <td><strong><?php echo $tweet->from->screen_name; ?></strong><small> from <?php echo $tweet->source; ?></small><br />
   <?php echo twitter_parse_tags($tweet->text); ?></td>
 </tr>
