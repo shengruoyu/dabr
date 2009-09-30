4601c9146b4d1002d7d51d9a60f175d43aee1833
david.carrington <david.carrington@760c5f7a-8625-11dd-9205-735403a6da14>
2009-09-30 12:22:18 +0000

    Time display for dabr_from_scratch tweets.

--- dabr_from_scratch/templates/mobile/timeline.tpl.php
+++ dabr_from_scratch/templates/mobile/timeline.tpl.php
@@ -1,9 +1,34 @@
+<?php
+
+function format_interval($timestamp, $granularity = 1) {
+  $units = array(
+    'years' => 31536000,
+    'days' => 86400,
+    'hours' => 3600,
+    'min' => 60,
+    'seconds' => 1
+  );
+  $output = '';
+  foreach ($units as $key => $value) {
+    if ($timestamp >= $value) {
+      $output .= ($output ? ' ' : '').floor($timestamp / $value).' '.$key;
+      $timestamp %= $value;
+      $granularity--;
+    }
+    if ($granularity == 0) {
+      break;
+    }
+  }
+  return $output ? $output : '0 seconds';
+}
+
+?>
 <table class="timeline">
 <tbody>
 <?php foreach ($timeline as $tweet): ?>
 <tr class="<?php echo ($i++ %2 ? 'even' : 'odd'); ?>">
   <td><img src="<?php echo $tweet->from->profile_image_url ?>" alt="" height="24" width="24" /></td>
-  <td><strong><?php echo $tweet->from->screen_name; ?></strong><small> from <?php echo $tweet->source; ?></small><br />
+  <td><strong><a href="user/<?php echo $tweet->from->screen_name; ?>"><?php echo $tweet->from->screen_name; ?></a></strong> <small><?php echo format_interval(time() - strtotime($tweet->created_at)); ?> ago from <?php echo $tweet->source; ?></small><br />
   <?php echo twitter_parse_tags($tweet->text); ?></td>
 </tr>
 <?php endforeach; ?>
