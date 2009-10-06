a97a21aff34d07eb0d914dfd895bf86e7668500e
david.carrington <david.carrington@760c5f7a-8625-11dd-9205-735403a6da14>
2009-10-06 20:41:13 +0000

    Added basic Twitter fail-whale handling, a character counter, and a simple login template.

---
+++ dabr_from_scratch/templates/mobile/login.tpl.php
@@ -0,0 +1,17 @@
+<div style="margin: 1em auto; width: 40em;">
+
+<h1>Dabr v2?</h1>
+
+<ul>
+<li>Dabr v2 is a <a href="http://code.google.com/p/dabr/wiki/Roadmap" title="Dabr v2 roadmap">complete rewrite</a> of the code from v1.</li>
+<li>You're looking at an early beta that's only <strong><?php $todo = simplexml_load_file('dabr.tdl'); $task = $todo->xpath('//TASK[@ID=1]'); echo $task[0]['CALCPERCENTDONE']; ?>%</strong> finished.</li>
+<li>This site is not a demo of any fancy new look or features, it's a site to check "does it actually work?".</li>
+</ul>
+
+<p>There's not even a login page yet, you'll have to use OAuth instead:</p>
+
+<p><a href="oauth"><em style="font-size:150%">Let's go!</em></a></p>
+
+<p>PS - ignore the numbers down the bottom</p>
+
+</div>
--- dabr_from_scratch/templates/mobile/update_form.tpl.php
+++ dabr_from_scratch/templates/mobile/update_form.tpl.php
@@ -1,5 +1,13 @@
 <form method="post" action="update">
   <input type="hidden" name="in_reply_to_status_id" value="<?php echo $in_reply_to_status_id; ?>" />
   <textarea id="status" name="status" rows="3" style="width:100%; max-width: 400px;"><?php echo $status; ?></textarea><br />
-  <input type="submit" value="Update" />
-</form>
+  <input type="submit" value="Update" /> <span id="remaining">140</span>
+</form> 
+<script type="text/javascript">
+function updateCount() 
+{
+  document.getElementById("remaining").innerHTML = 140 - document.getElementById("status").value.length;
+  setTimeout(updateCount, 400);
+}
+updateCount();
+</script>
