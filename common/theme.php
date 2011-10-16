a5be51d3273de53f6976b93a1a79f00611dc1cc6
terence.eden <terence.eden@gmail.com>
2011-10-16 19:36:55 +0000

    Big check in... Several main changes 1) Rationalised the head, body, and footer generation - only in one location. Makes it a lot easier to make changes. HTML5 DOCTYPE - just for fun ;-) 2) Lots of other functions rationalised 3) Added "Pretty In Pink" as a theme! Set as the default with a message telling people to visit the settings if they've never been there. 4) Search now does geo-search 5) Probably some other stuff....

--- common/theme.php
+++ common/theme.php
@@ -132,78 +132,82 @@ function theme_page($title, $content) {
 	$body .= theme('menu_bottom');
 	$body .= theme('google_analytics');
 	if (DEBUG_MODE == 'ON') {
-		global $dabr_start, $api_time, $services_time;
+		global $dabr_start, $api_time, $services_time, $rate_limit;
 		$time = microtime(1) - $dabr_start;
 		$body .= '<p>Processed in '.round($time, 4).' seconds ('.round(($time - $api_time - $services_time) / $time * 100).'% Dabr, '.round($api_time / $time * 100).'% Twitter, '.round($services_time / $time * 100).'% other services)</p>';
 	}
 	if ($title == 'Login') {
-		$title = 'mobile Twitter Login';
+		$title = 'Dabr - mobile Twitter Login';
 		$meta = '<meta name="description" content="Free open source alternative to mobile Twitter, bringing you the complete Twitter experience to your phone." />';
 	}
 	ob_start('ob_gzhandler');
 	header('Content-Type: text/html; charset=utf-8');
-	echo	'<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-			<html xmlns="http://www.w3.org/1999/xhtml">
-			<head>
-				<title>',$_SERVER['SERVER_NAME'],' - ',$title,'</title>
-				<base href="',BASE_URL,'" />
-				'.$meta.theme('css').'
-				<meta name="viewport" content="width=device-width; initial-scale=1.0;" />
-			</head>
-			<body>';
-
-	echo 	theme('advert');
-	echo 	$body;
-
-	echo	'</body>
-		</html>';
+	echo	'<!DOCTYPE html>
+				<html>
+					<head>
+						<meta charset="utf-8" />
+						<meta name="viewport" content="width=device-width; initial-scale=1.0;" />
+						<title>Dabr - ' . $title . '</title>
+						<base href="',BASE_URL,'" />
+						'.$meta.theme('css').'
+					</head>
+					<body id="thepage">';
+	echo 				"<div id=\"advert\">" . show_advert() . "</div>"; //theme('advert');
+	echo 				$body;
+	if (setting_fetch('colours') == null)
+	{
+		//	If the cookies haven't been set, remind the user that they can set how Dabr looks
+		echo			'<p>Think Dabr looks ugly? <a href="settings">Change the colours!</a></p>';
+	}
+	echo '		</body>
+				</html>';
 	exit();
 }
 
 function theme_colours() {
-	$info = $GLOBALS['colour_schemes'][setting_fetch('colours', 5)];
+	$info = $GLOBALS['colour_schemes'][setting_fetch('colours', 0)];
 	list($name, $bits) = explode('|', $info);
 	$colours = explode(',', $bits);
 	return (object) array(
-		'links' => $colours[0],
-		'bodybg' => $colours[1],
-		'bodyt' => $colours[2],
-		'small' => $colours[3],
-		'odd' => $colours[4],
-		'even' => $colours[5],
-		'replyodd' => $colours[6],
-		'replyeven' => $colours[7],
-		'menubg' => $colours[8],
-		'menut' => $colours[9],
-		'menua' => $colours[10],
+		'links'		=> $colours[0],
+		'bodybg'		=> $colours[1],
+		'bodyt'		=> $colours[2],
+		'small'		=> $colours[3],
+		'odd'			=> $colours[4],
+		'even'		=> $colours[5],
+		'replyodd'	=> $colours[6],
+		'replyeven'	=> $colours[7],
+		'menubg'		=> $colours[8],
+		'menut'		=> $colours[9],
+		'menua'		=> $colours[10],
 	);
 }
 
 function theme_css() {
 	$c = theme('colours');
 	return "<style type='text/css'>
-  a{color:#{$c->links}}
-  table{border-collapse:collapse}
-  form{margin:.3em;}
-  td{vertical-align:top;padding:0.3em}
-  img{border:0}
-  small,small a{color:#{$c->small}}
-  body{background:#{$c->bodybg};
-  color:#{$c->bodyt};margin:0;font:90% sans-serif}
-  .odd{background:#{$c->odd}}
-  .even{background:#{$c->even}}
-  .reply{background:#{$c->replyodd}}
-  .reply.even{background: #{$c->replyeven}}
-  .menu{color:#{$c->menut};background:#{$c->menubg};padding: 2px}
-  .menu a{color:#{$c->menua};text-decoration: none}
-  .tweet,.features{padding:5px}
-  .date{padding:5px;font-size:0.8em;font-weight:bold;color:#{$c->small}}
-  .about,.time{font-size:0.75em;color:#{$c->small}}
-  .avatar{display:block; height:26px; width:26px; left:0.3em; margin:0; overflow:hidden; position:absolute;}
-  .status{display:block;word-wrap:break-word;}
-  .shift{margin-left:30px;min-height:24px;}
-  .from{font-size:0.75em;color:#{$c->small};font-family:serif;}
-  .from a{color:#{$c->small};}
+	a{color:#{$c->links}}
+	table{border-collapse:collapse}
+	form{margin:.3em;}
+	td{vertical-align:top;padding:0.3em}
+	img{border:0}
+	small,small a{color:#{$c->small}}
+	body{background:#{$c->bodybg};
+	color:#{$c->bodyt};margin:0;font:90% sans-serif}
+	.odd{background:#{$c->odd}}
+	.even{background:#{$c->even}}
+	.reply{background:#{$c->replyodd}}
+	.reply.even{background: #{$c->replyeven}}
+	.menu{color:#{$c->menut};background:#{$c->menubg};padding: 2px}
+	.menu a{color:#{$c->menua};text-decoration: none}
+	.tweet,.features{padding:5px}
+	.date{padding:5px;font-size:0.8em;font-weight:bold;color:#{$c->small}}
+	.about,.time{font-size:0.75em;color:#{$c->small}}
+	.avatar{display:block; height:26px; width:26px; left:0.3em; margin:0; overflow:hidden; position:absolute;}
+	.status{display:block;word-wrap:break-word;}
+	.shift{margin-left:30px;min-height:24px;}
+	.from{font-size:0.75em;color:#{$c->small};font-family:serif;}
+	.from a{color:#{$c->small};}
 </style>";
 }
 
