361cf808624e6e45fe35139725b416c1a33b2cff
terence.eden@gmail.com <terence.eden@gmail.com@760c5f7a-8625-11dd-9205-735403a6da14>
2012-05-25 12:35:11 +0000

    Made search's "in reply to" more consistent with regular api

--- trunk/common/twitter.php
+++ trunk/common/twitter.php
@@ -1806,7 +1806,7 @@ function twitter_standard_timeline($feed, $source) {
 					'entities' => $status->entities,
 					'in_reply_to_status_id' => $status->in_reply_to_status_id,
 					'in_reply_to_status_id_str' => $status->in_reply_to_status_id_str,
-					'in_reply_to_screen_name' => $status->to_user_name,
+					'in_reply_to_screen_name' => $status->to_user,
 				);
 			}
 			return $output;
