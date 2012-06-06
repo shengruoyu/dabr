6afa108740ea2b56a1aaf91f9c5a6de09f57618b
terence.eden@gmail.com <terence.eden@gmail.com@760c5f7a-8625-11dd-9205-735403a6da14>
2012-06-06 16:27:26 +0000

    Twitter have a new logo they want us to use https://dev.twitter.com/blog/new-day-new-bird

--- trunk/browsers/desktop.php
+++ trunk/browsers/desktop.php
@@ -1,12 +1,7 @@
 <?php
 function desktop_theme_status_form($text = '', $in_reply_to_id = NULL) {
 	if (user_is_authenticated()) {
-		if ($_SERVER['HTTPS'] == "on") {
-			$icon = "https://si0.twimg.com/images/dev/cms/intents/bird/bird_blue/bird_16_blue.png";
-		}
-		else {
-			$icon = "http://a1.twimg.com/images/dev/cms/intents/bird/bird_blue/bird_16_blue.png";
-		}
+		$icon = "https://twitter.com/images/three_circles/twitter-bird-16x16.png";
 		
 		//	adding ?status=foo will automaticall add "foo" to the text area.
 		if ($_GET['status'])
