9d53da4c30f7df5cc68b67d94bc399fbfa37f421
ryancullen <ryancullen@760c5f7a-8625-11dd-9205-735403a6da14>
2013-03-21 22:09:55 +0000

    Fixed bug with lists where a user was either a member of none, or followed none.

--- trunk/common/lists.php
+++ trunk/common/lists.php
@@ -7,16 +7,6 @@ menu_register(array(
 	),
 ));
 
-
-
-/*
- API Calls
-
- Note that some calls are XML and not JSON like the rest of Dabr. This is because 32-bit
- PHP installs cannot handle the 64-bit Lists API cursors used for paging.
-
- */
-
 function lists_paginated_process($url) {
 	// Adds cursor/pagination parameters to a query
 	$cursor = $_GET['cursor'];
@@ -169,12 +159,14 @@ function lists_list_subscribers_page($user, $list) {
 /* Theme functions */
 
 function theme_lists($json) {
-	if($json->lists)
+	if(isset($json->lists)) {
 		$lists = $json->lists;
-	else
+	}
+	else {
 		$lists = $json;
-	if (count($lists) == 0) {
-		return "<p>No lists to display</p><pre>" . print_r($json,1) . "</pre>";
+	}
+	if (sizeof($lists) == 0 || $lists == '[]') {
+		return "<p>No lists to display</p>";
 	}
 	$rows = array();
 	$headers = array('List ', 'Members ', 'Subscribers');
