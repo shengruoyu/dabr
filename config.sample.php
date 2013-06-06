4d9b7fd9b4ed39b18ab8f69d5ecbd45950c8e12c
ryancullen <ryancullen@gmail.com>
2013-06-06 10:43:57 +0000

    Removed value from the IMAGE_PROXY_URL, also highlighted that the https://tinysrc.appspot.com/ no longer works.

--- config.sample.php
+++ config.sample.php
@@ -7,8 +7,8 @@ define('API_URL','http://api.twitter.com/1/');
 
 // Image Proxy URL
 // Use http://src.sencha.io/ for regular connections
-// Use https://tinysrc.appspot.com/ for SSL connections
-define('IMAGE_PROXY_URL', 'https://tinysrc.appspot.com/');
+// Use https://tinysrc.appspot.com/ for SSL connections (no longer appears to work)
+define('IMAGE_PROXY_URL', '');
 
 // Cookie encryption key. Max 52 characters
 define('ENCRYPTION_KEY', 'Example Key - Change Me!');
