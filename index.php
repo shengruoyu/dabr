159d3289c922a590bde14c926c1b75301c96d97e
ryancullen <ryancullen@gmail.com>
2014-01-07 20:43:49 +0000

    Twitter is ending support for http requests, blackout test today resulted in 403s for all requests. https://dev.twitter.com/discussions/24239

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ require 'common/lists.php';
 require 'common/settings.php';
 
 // Twitter's API URL.
-define('API_NEW','http://api.twitter.com/1.1/');
-define('API_OLD','http://api.twitter.com/1/');
+define('API_NEW','https://api.twitter.com/1.1/');
+define('API_OLD','https://api.twitter.com/1/');
 
 menu_register(array (
 	'about' => array (
